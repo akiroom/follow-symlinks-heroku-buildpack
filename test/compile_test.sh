37e77fe775bea3b164984140d815c602cdf41587
Hiroki Akiyama <akiyama@akiroom.com>
2020-03-01 16:56:44 +0900

    add new test

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,12 +2,9 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-cd ${BUILDPACK_HOME}
-./scripts/generate-sample-symbolic-link
-#echo $(pwd)
-#echo $(ls -la)
-
 testPrepared () {
+  cd ${BUILDPACK_HOME}
+  ./scripts/generate-sample-symbolic-link
   SOURCE_DIR=./example/source_dir
   DEST_SYM_LINK=./example/dest_dir/source_dir
   IGNORE_SYM_LINK=./example/dest_dir/ignored_dir/source_dir
@@ -16,4 +13,10 @@ testPrepared () {
   assertTrue "Should have directory ${SOURCE_DIR}" "[ -d ${SOURCE_DIR} ]"
   assertTrue "Should have symbolic link ${DEST_SYM_LINK}" "[ -h ${DEST_SYM_LINK} ]"
   assertTrue "Should have symbolic link ${IGNORE_SYM_LINK}" "[ -h ${IGNORE_SYM_LINK} ]"
+}
+
+testCompile () {
+  cd ${BUILDPACK_HOME}
+  SOURCE_DIR=./example/dest_dir/source_dir
+  assertTrue "Should have new actual files ${SOURCE_DIR}" "[ -d ${SOURCE_DIR} ]"
 }
