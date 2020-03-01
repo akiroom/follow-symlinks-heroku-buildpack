a1ee01f761ea974d45e699fffe00da1090396446
Hiroki Akiyama <akiyama@akiroom.com>
2020-03-01 16:50:35 +0900

    hoge

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,11 +2,12 @@
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-${BUILDPACK_HOME}/scripts/generate-sample-symbolic-link
+cd ${BUILDPACK_HOME}
+./scripts/generate-sample-symbolic-link
 echo $(pwd)
 echo $(ls -la)
 
 testPrepared () {
-  SOURCE_DIR=${BUILDPACK_HOME}/example/source_dir
-  assertTrue "Should have symbolic link ${SOURCE_DIR}`" "[ -f ${SOURCE_DIR} ]"
+  SOURCE_DIR=./example/source_dir
+  assertTrue "Should have symbolic link ${SOURCE_DIR}" "[ -f ${SOURCE_DIR} ]"
 }
