3c7c25068b24bc0f1ae6d2ec64785a3d5022f02a
Hiroki Akiyama <akiyama@akiroom.com>
2020-03-01 16:54:10 +0900

    hoge

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -9,7 +9,11 @@ cd ${BUILDPACK_HOME}
 
 testPrepared () {
   SOURCE_DIR=./example/source_dir
+  DEST_SYM_LINK=./example/dest_dir/source_dir
+  IGNORE_SYM_LINK=./example/ignored_dir/source_dir
 
   # -L option requires symbolic link
-  assertTrue "Should have symbolic link ${SOURCE_DIR}" "[ -L ${SOURCE_DIR} ]"
+  assertTrue "Should have directory ${SOURCE_DIR}" "[ -d ${SOURCE_DIR} ]"
+  assertTrue "Should have symbolic link ${DEST_SYM_LINK}" "[ -h ${DEST_SYM_LINK} ]"
+  assertTrue "Should have symbolic link ${IGNORE_SYM_LINK}" "[ -h ${IGNORE_SYM_LINK} ]"
 }
